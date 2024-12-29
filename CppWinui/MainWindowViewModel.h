2b0633aa41f427c400c7af57ef66883e3da84025
秋风Elipese <1175971310@qq.com>
2024-12-29 18:18:07 +0800

    1111

--- CppWinui/MainWindowViewModel.h
+++ CppWinui/MainWindowViewModel.h
@@ -15,16 +15,16 @@ namespace winrt::CppWinui::implementation
     private:
 		int32_t m_myProperty{ 0 };
     public:
-        event_token PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& value) {
+        event_token PropertyChanged(Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& value) {
             return m_propertyChanged.add(value);
         }
         void PropertyChanged(event_token const& token) {
             m_propertyChanged.remove(token);
         }
     private:
-        event<Windows::UI::Xaml::Data::PropertyChangedEventHandler> m_propertyChanged; 
+        event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> m_propertyChanged; 
         void RaisePropertyChanged(hstring const& propertyName) {
-            m_propertyChanged(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs(propertyName));
+            m_propertyChanged(*this, Microsoft::UI::Xaml::Data::PropertyChangedEventArgs(propertyName));
         }
     };
 }
